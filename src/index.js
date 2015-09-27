698ae5445ad28846bc6361852458810b5f5ee44f
Tejas Manohar <me@tejas.io>
2015-09-27 03:05:47 -0500

    use default parametesr to eliminate - blah = x || y logic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import _ from 'lodash';
 
 export default function(schema, options = {}) {
-  schema.methods.update = async function(source, permitted) {
-    permitted = permitted || options.permitted;
+  schema.methods.update = async function(source, permitted = options.permitted) {
     Object.assign(this, permitted ? _.pick(source, permitted) : source);
     return (await this.save())[0];
   }
